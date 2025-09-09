c42a885f32c39f01963382556e99e978d452883a
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-09-09 15:09:59 +0530

    read all file formate json and excel

--- 1_reaad_data/read.py
+++ 1_reaad_data/read.py
@@ -3,5 +3,10 @@ import pandas as pd
 
 # read data from CSV fil into a dataframe
 
-df=pd.read_csv(r"D:\D\ML-DL-AI\Pandas\1_reaad_data\sales_data_sample.csv" , encoding="latin1")
+# df=pd.read_csv(r"D:\D\ML-DL-AI\Pandas\1_reaad_data\sales_data_sample.csv" , encoding="latin1")
+
+
+# df = pd.read_excel(r"D:\D\ML-DL-AI\Pandas\1_reaad_data\SampleSuperstore.xlsx")
+
+df = pd.read_json(r"D:\D\ML-DL-AI\Pandas\1_reaad_data\sample_Data.json")
 print(df)
