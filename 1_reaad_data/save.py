0a6281295be8dfc88adf461f18a39cdd42d812bc
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-09-10 14:54:24 +0530

    Save JSON File
    A dictonary date to save JSON file

--- 1_reaad_data/save.py
+++ 1_reaad_data/save.py
@@ -13,4 +13,5 @@ df=pd.DataFrame(data)
 print(df)
 
 # df.to_csv("output.csv", index=False)
-df.to_excel("output.xlsx" , index=False)
+# df.to_excel("output.xlsx" , index=False)
+df.to_json("output.json" , indent=False)
